80a4e422da82c6707e5484f1a1dd3b084a2c6846
Vaibhav <vaibhavchaudhary144@gmail.com>
2023-05-13 11:00:57 +0530

    More added!

--- 282A.cc
+++ 282A.cc
@@ -4,6 +4,24 @@ int INF = 1e9;
 
 int main()
 {
+    int x = 0;
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin>>s;
+        if (s[1]=='+')
+        {
+            x++;
+        }
+        if (s[1]=='-')
+        {
+            x--;
+        }
+        
+    }
+    cout<<x;
     
     return 0;
 }
