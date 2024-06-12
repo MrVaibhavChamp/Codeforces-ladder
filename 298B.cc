c439e23045803001ad4e5da902c9f86e1e084cba
Vaibhav <vaibhavchaudhary144@gmail.com>
2024-06-12 23:41:53 +0530

    Some edits!

--- 298B.cc
+++ 298B.cc
@@ -1,132 +1,50 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
-#define ll long long
-#define endl '\n'
-#define mod 1000000007
-int INF = 1e9;
 
-void solve()
+int main()
 {
-    ll t, x1, y1, x2, y2;
-    cin >> t >> x1 >> y1 >> x2 >> y2;
-    ll steps_x = x2 - x1;
-    ll steps_y = y2 - y1;
-
-    if (steps_x >= 0 && steps_y >= 0)
+    int t, sx, sy, ex, ey, time(-1);
+    string wind;
+    cin >> t >> sx >> sy >> ex >> ey >> wind;
+    for (int i = 0; i < t; ++i)
     {
-        steps_x = abs(steps_x);
-        steps_y = abs(steps_y);
-        for (ll i = 0; i < t; i++)
+        switch (wind[i])
         {
-            char ch;
-            cin >> ch;
-            if (ch == 'N' && steps_y > 0)
+        case 'E':
+            if (sx < ex)
             {
-                steps_y--;
+                sx += 1;
             }
-            if (ch == 'E' && steps_x > 0)
+            break;
+        case 'S':
+            if (sy > ey)
             {
-                steps_x--;
+                sy -= 1;
             }
-            if (steps_x == 0 && steps_y == 0)
+            break;
+        case 'W':
+            if (sx > ex)
             {
-                cout << i + 1;
-                return;
+                sx -= 1;
             }
+            break;
+        case 'N':
+            if (sy < ey)
+            {
+                sy += 1;
+            }
+            break;
+        default:
+            break;
         }
-
-        cout << "-1";
-    }
-    else if (steps_x >= 0 && steps_y <= 0)
-    {
-        steps_x = abs(steps_x);
-        steps_y = abs(steps_y);
-        for (ll i = 0; i < t; i++)
+        if (sx == ex && sy == ey)
         {
-            char ch;
-            cin >> ch;
-            if (ch == 'S' && steps_y > 0)
-            {
-                steps_y--;
-            }
-            if (ch == 'E' && steps_x > 0)
-            {
-                steps_x--;
-            }
-            if (steps_x == 0 && steps_y == 0)
-            {
-                cout << i + 1;
-                return;
-            }
+            time = i + 1;
+            break;
         }
-
-        cout << "-1";
     }
-    else if (steps_x <= 0 && steps_y >= 0)
-    {
-        steps_x = abs(steps_x);
-        steps_y = abs(steps_y);
-        for (ll i = 0; i < t; i++)
-        {
-            char ch;
-            cin >> ch;
-            if (ch == 'N' && steps_y > 0)
-            {
-                steps_y--;
-            }
-            if (ch == 'W' && steps_x > 0)
-            {
-                steps_x--;
-            }
-            if (steps_x == 0 && steps_y == 0)
-            {
-                cout << i + 1;
-                return;
-            }
-        }
-
-        cout << "-1";
-    }
-    else if (steps_x <= 0 && steps_y <= 0)
-    {
-        steps_x = abs(steps_x);
-        steps_y = abs(steps_y);
-        for (ll i = 0; i < t; i++)
-        {
-            char ch;
-            cin >> ch;
-            if (ch == 'S' && steps_y > 0)
-            {
-                steps_y--;
-            }
-            if (ch == 'W' && steps_x > 0)
-            {
-                steps_x--;
-            }
-            if (steps_x == 0 && steps_y == 0)
-            {
-                cout << i + 1;
-                return;
-            }
-        }
-
-        cout << "-1";
-    }
-}
-
-signed main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    ll test = 1;
-    // cin>>test;
-
-    while (test--)
-    {
-        solve();
-    }
-
+    cout << time << endl;
     return 0;
 }
